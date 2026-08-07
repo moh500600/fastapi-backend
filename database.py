1084e75bc1e73cbde72bbac05ce437aa1b068911
krona11 <mohgoat452@gmail.com>
2026-08-07 21:34:49 +0300

    hu

--- database.py
+++ database.py
@@ -5,26 +5,34 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = URL.create(
-    drivername="mysql+pymysql",
-    username=os.environ.get("MYSQL_USER", "avnadmin"),
-    password=os.environ.get("MYSQL_PASSWORD", "AVNS_kLgxK_HL0S6OZOQ3mYx"),
-    host=os.environ.get("MYSQL_HOST", "fastapi-mysql-fastapi-project-goat.a.aivencloud.com"),
-    port=int(os.environ.get("MYSQL_PORT", 13591)),
-    database=os.environ.get("MYSQL_DATABASE", "defaultdb"),
-    query={"charset": "utf8mb4"},
-)
-
+# --- Temporarily using SQLite ---
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=300,
-    connect_args={
-        "connect_timeout": 10,
-        "ssl": {}, # Required for Aiven
-    },
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# --- Original MySQL configuration (Commented out) ---
+# SQLALCHEMY_DATABASE_URL = URL.create(
+#     drivername="mysql+pymysql",
+#     username=os.environ.get("MYSQL_USER", "avnadmin"),
+#     password=os.environ.get("MYSQL_PASSWORD", "AVNS_kLgxK_HL0S6OZOQ3mYx"),
+#     host=os.environ.get("MYSQL_HOST", "fastapi-mysql-fastapi-project-goat.a.aivencloud.com"),
+#     port=int(os.environ.get("MYSQL_PORT", 13591)),
+#     database=os.environ.get("MYSQL_DATABASE", "defaultdb"),
+#     query={"charset": "utf8mb4"},
+# )
+# 
+# engine = create_engine(
+#     # SQLALCHEMY_DATABASE_URL, # Needs to be uncommented along with the URL creation above
+#     "mysql+pymysql://avnadmin:AVNS_kLgxK_HL0S6OZOQ3mYx@fastapi-mysql-fastapi-project-goat.a.aivencloud.com:13591/defaultdb?charset=utf8mb4",
+#     pool_pre_ping=True,
+#     pool_recycle=300,
+#     connect_args={
+#         "connect_timeout": 10,
+#         "ssl": {}, # Required for Aiven
+#     },
+# )
 
 
 SessionLocal = sessionmaker(
